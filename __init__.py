48a730e000403fc84f0fac4b94a6c889edbb8f13
JonahY <binchengy@163.com>
2022-05-14 21:24:00 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+@version: 1.0
+@author: Jonah
+@file: StreamToAscii.py
+@Created time: 2022/05/14 20:00
+@Last Modified: 2022/05/14 21:22
+"""
+
+'''
+# Step for Pyinstall
+pyinstaller -F -c -i logo.ico StreamToAscii.py -n StreamToAscii(Parallel)
+'''
